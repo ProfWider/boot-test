f7a938472ee4fff4a1b7a3c25011ce5ea5517a99
Prof. Dr. Arif Wider <wider@htw-berlin.de>
2021-04-28 17:35:10 +0200

    reading env variable

--- src/main/java/com/arifwider/boottest/HelloController.java
+++ src/main/java/com/arifwider/boottest/HelloController.java
@@ -1,14 +1,20 @@
 package com.arifwider.boottest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private Environment env;
+
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Webtech";
+        String testValue = env.getProperty("TEST_VALUE");
+        return "Greetings from Webtech! (" + (testValue == null ? "Environment variable not found" : testValue) + ")";
     }
 
 }
